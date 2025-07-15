c8e9eeac61abd43d29799d01d910be88f85c282d
rosy <lc_rosy@qq.com>
2025-07-15 17:37:19 +0800

    二端队列性能测试

---
+++ utils/benches/bench_deque.rs
@@ -0,0 +1,203 @@
+use std::{
+    collections::VecDeque,
+    os::unix::thread,
+    sync::{Arc, Mutex},
+    thread::spawn,
+};
+
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use utils::LockFreeDeque;
+
+fn test() {
+    let deque: LockFreeDeque<usize, 100> = LockFreeDeque::new();
+    for i in 0..99 {
+        deque.push_back(i);
+    }
+    for i in 0..99 {
+        assert_eq!(deque.pop_front(), Some(i));
+    }
+}
+
+fn lock_free_se(data_num: usize, thread_num: usize) {
+    // assert!(thread_num % 4 == 0);
+    // assert!(data_num * thread_num / 2 <= 10000);
+    let deque: Arc<LockFreeDeque<usize, 10001>> = Arc::new(LockFreeDeque::new());
+    let mut handles = vec![];
+    for _ in 0..(thread_num / 2) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            for _ in 0..data_num {
+                deque_clone.push_back(1);
+            }
+        }));
+    }
+    for _ in 0..(thread_num / 2) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            let mut i = 0;
+            while i < data_num {
+                if deque_clone.pop_front().is_some() {
+                    i += 1;
+                }
+            }
+        }));
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+fn lock_free_de(data_num: usize, thread_num: usize) {
+    // assert!(thread_num % 4 == 0);
+    // assert!(data_num * thread_num / 2 <= 10000);
+    let deque: Arc<LockFreeDeque<usize, 10001>> = Arc::new(LockFreeDeque::new());
+    let mut handles = vec![];
+    for _ in 0..(thread_num / 4) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            for _ in 0..data_num {
+                deque_clone.push_front(1);
+            }
+        }));
+    }
+    for _ in 0..(thread_num / 4) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            for _ in 0..data_num {
+                deque_clone.push_back(1);
+            }
+        }));
+    }
+    for _ in 0..(thread_num / 4) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            let mut i = 0;
+            while i < data_num {
+                if deque_clone.pop_front().is_some() {
+                    i += 1;
+                }
+            }
+        }));
+    }
+    for _ in 0..(thread_num / 4) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            let mut i = 0;
+            while i < data_num {
+                if deque_clone.pop_back().is_some() {
+                    i += 1;
+                }
+            }
+        }));
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+fn mutex_se(data_num: usize, thread_num: usize) {
+    // assert!(thread_num % 4 == 0);
+    // assert!(data_num * thread_num / 2 <= 10000);
+    let deque: Arc<Mutex<VecDeque<usize>>> = Arc::new(Mutex::new(VecDeque::new()));
+    let mut handles = vec![];
+    for _ in 0..(thread_num / 2) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            for _ in 0..data_num {
+                deque_clone.lock().unwrap().push_back(1);
+            }
+        }));
+    }
+    for _ in 0..(thread_num / 2) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            let mut i = 0;
+            while i < data_num {
+                if deque_clone.lock().unwrap().pop_front().is_some() {
+                    i += 1;
+                }
+            }
+        }));
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+fn mutex_de(data_num: usize, thread_num: usize) {
+    // assert!(thread_num % 4 == 0);
+    // assert!(data_num * thread_num / 2 <= 10000);
+    let deque: Arc<Mutex<VecDeque<usize>>> = Arc::new(Mutex::new(VecDeque::new()));
+    let mut handles = vec![];
+    for _ in 0..(thread_num / 4) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            for _ in 0..data_num {
+                deque_clone.lock().unwrap().push_front(1);
+            }
+        }));
+    }
+    for _ in 0..(thread_num / 4) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            for _ in 0..data_num {
+                deque_clone.lock().unwrap().push_back(1);
+            }
+        }));
+    }
+    for _ in 0..(thread_num / 4) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            let mut i = 0;
+            while i < data_num {
+                if deque_clone.lock().unwrap().pop_front().is_some() {
+                    i += 1;
+                }
+            }
+        }));
+    }
+    for _ in 0..(thread_num / 4) {
+        let deque_clone = deque.clone();
+        handles.push(spawn(move || {
+            let mut i = 0;
+            while i < data_num {
+                if deque_clone.lock().unwrap().pop_back().is_some() {
+                    i += 1;
+                }
+            }
+        }));
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("bench_deque_de");
+    let data_num = 100;
+    for thread_num in [4, 8, 16, 32, 64].iter() {
+        group.bench_with_input(
+            BenchmarkId::new("LockFreeDeque-DoubleEnded", thread_num),
+            thread_num,
+            |b, i| b.iter(|| lock_free_de(data_num, *i)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("Mutex+VecDeque-DoubleEnded", thread_num),
+            thread_num,
+            |b, i| b.iter(|| mutex_de(data_num, *i)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("LockFreeDeque-SingleEnded", thread_num),
+            thread_num,
+            |b, i| b.iter(|| lock_free_se(data_num, *i)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("Mutex+VecDeque-SingleEnded", thread_num),
+            thread_num,
+            |b, i| b.iter(|| mutex_se(data_num, *i)),
+        );
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
