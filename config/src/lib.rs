a934d44499a0a34000f28a12a1b7310d1c7cf58c
zflcs <1491657576@qq.com>
2025-07-21 20:12:22 +0800

    Modify the task stack size.

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -8,6 +8,6 @@ mod mut_cfgs {
 pub use mut_cfgs::*;
 
 pub const DATA_SEC_MASK: usize = 0xFFFF_FFFF_FFFF_F000;
-pub const TASK_STACK_SIZE: usize = 0x4000;
+pub const TASK_STACK_SIZE: usize = 0x40000;
 pub const PAGES_SIZE_4K: usize = 0x1000;
 pub type AxCpuMask = cpumask::CpuMask<SMP>;
